f4dce79bac7a183813851ce6525ad57afe38786e
Yaroslav Yakupov <yakupovyaroslav@icloud.com>
2024-12-24 22:01:53 +0300

    TEST

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from "@originjs/vite-plugin-federation";
+export default defineConfig({
+    preview: {
+        host: 'localhost',
+        port: 5001,
+        strictPort: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
+    },
+    server: {
+        port: 5001,
+    },
+    plugins: [
+        react(),
+        federation({
+            name: 'vite-federation-test-two',
+            filename: 'vite_federation_test_two.js',
+            exposes: {
+                './SecondAppComponent': './src/components/SecondAppComponent.tsx',
+            },
+            shared: ['react', 'react-dom']
+        })
+    ],
+    build: {
+        target: 'esnext',
+        minify: false,
+        cssCodeSplit: false
+    }
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  preview: {
+    host: 'localhost',
+    port: 5001,
+    strictPort: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
+  server: {
+    port: 5001,
+  },
+  plugins: [
+    react(), 
+    federation({
+      name: 'vite-federation-test-two',
+      filename: 'vite_federation_test_two.js',
+      exposes: {
+          './SecondAppComponent': './src/components/SecondAppComponent.tsx',
+      },
+      shared: ['react', 'react-dom']
+    }),
+    tsconfigPaths(),
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
